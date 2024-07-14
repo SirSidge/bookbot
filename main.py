0d01148e5c88f3c3632c40fb1d3439aa0c4561f3
SirSidge <heinz.hscharlach@gmail.com>
2024-07-14 20:00:21 +0200

    added function to count each letter

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ def main():
     text = get_text(book_path)
     num_words = get_num_words(text)
     print(f"{num_words} words found in the document")
+    dict_letters = get_num_letters(text)
+    print(dict_letters)
 
 def get_text(path):
     with open(path) as f:
@@ -11,5 +13,15 @@ def get_text(path):
 def get_num_words(text):
     words = text.split()
     return len(words)
+
+def get_num_letters(text):
+    dict_letters = {}
+    for letter in text:
+        lowered = letter.lower()
+        if letter.lower() in dict_letters:
+            dict_letters[lowered] += 1
+        else:
+            dict_letters[lowered] = 1
+    return dict_letters
 
 main()
