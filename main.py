2f8986ffe11d3810c29a348d3d7d0bbccf728e61
SirSidge <heinz.hscharlach@gmail.com>
2024-07-15 19:20:13 +0200

    added func to sort list

--- main.py
+++ main.py
@@ -4,12 +4,10 @@ def main():
     text = get_text(book_path)
     global num_words
     num_words = get_num_words(text)
-    #print(f"{num_words} words found in the document")
+    global my_alphabet
+    my_alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
     dict_letters = get_num_letters(text)
-    #print(dict_letters)
     get_report(dict_letters)
-
-
 
 def get_text(path):
     with open(path) as f:
@@ -29,29 +27,22 @@ def get_num_letters(text):
             dict_letters[lowered] = 1
     return dict_letters
 
-#def sort_on(dict):
-#    return dict["num"]
-
-
-#============================================================================
-#NEED TO WORK ON 'get_report'. IT'S CLEAR THE PROBLEM LIES WITH THE SORT FUNCTION. IF YOU EXECUTE 'python3 main.py' you will get all the characters
-#but they're not organised either.
-#Figure out how to remove everything that isn't an alphnumeric character, remove them, then organise the rest according to "num" in the dictionary.
-#We might need to change the structure of li_dicts as this is should be written according to the List on boot.dev example.
-#============================================================================
+def sort_on(dict):
+    return dict["num"]
 
 def get_report(dict_letters):
     li_dicts = []
     for letter in dict_letters:
-        li_dicts.append({f"{letter}": dict_letters[letter]})
-    #li_dicts.sort(reverse=True, key=sort_on)
+        if letter in my_alphabet:
+            li_dicts.append({"letter": f"{letter}", "num": dict_letters[letter]})
+    li_dicts.sort(reverse=True, key=sort_on)
+    
     report_output = f"--- Begin report of {book_path} ---\n{num_words} words found in the document\n\n"
     for dict in li_dicts:
-        for i in dict:
-            report_output += f"The '{i}' character was found {dict[i]} times\n"
+        temp_letter = dict["letter"]
+        temp_num = dict["num"]
+        report_output += f"The '{temp_letter}' character was found {temp_num} times\n"
     report_output += "--- End report ---"
     print(report_output)
-
-
 
 main()
