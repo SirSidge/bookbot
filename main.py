801851b0551b6aa45bf1382619510bb0df4e1c42
SirSidge <heinz.hscharlach@gmail.com>
2025-01-18 12:35:38 +0200

    added initial book report (un-ordered)

--- main.py
+++ main.py
@@ -3,8 +3,10 @@ def main():
     text = get_book_text(book_path)
     num_words = word_counter(text)
     num_chars = char_counter(text)
+    book_report = report(num_words, num_chars)
     #print(num_words)
-    print(num_chars)
+    #print(num_chars)
+    print(book_report)
 
 def get_book_text(book_path):
     with open(book_path) as f:
@@ -22,5 +24,13 @@ def char_counter(text):
         else:
             char_dict[char] = 1
     return char_dict
+
+def report(num_words, num_chars):
+    book_report = f"--- Begin report of books/frankenstein.txt ---\n{num_words} words found in document\n\n"
+    for key, value in num_chars.items():
+        if key.isalpha():
+            book_report += (f"The key is: {key}, and the value is: {value}\n")
+    book_report += "--- End report ---"
+    return book_report
 
 main()
